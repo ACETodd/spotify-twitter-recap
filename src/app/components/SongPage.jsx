d11be67fa4332245dc9941427f9c0f860d57df3c
Todd Hoover <toddinator@hooverview.com>
2026-04-14 20:53:42 -0500

    ag

--- src/app/components/SongPage.jsx
+++ src/app/components/SongPage.jsx
@@ -144,7 +144,7 @@ export default function SongPage({user, setUser}) {
                   </div>
                   <div 
                     key={currentTrack?.name || 'placeholder'} 
-                    className={`fixed bottom-[24rem] right-[12rem] text-right ${
+                    className={`fixed bottom-[25rem] right-[11rem] text-right ${
                       fadeState === "fadeIn" ? "animate-fadeIn" : "animate-fadeOut"
                     }`}
                     style={{maxWidth: 525}}
